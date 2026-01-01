3028da6545cc6d71b23c2bd2e656dc2617f9f8cd
kabilanero <kapilrhode0000@gmail.com>
2026-01-01 23:58:05 +0530

    commited adding but start script in packagejson

--- src/config/mail.js
+++ src/config/mail.js
@@ -3,9 +3,12 @@ dotenv.config();   // <--- ADD THIS
  import nodemailer from "nodemailer";
 
 export const mailTransporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,        // keep this
+  secure: false,    // true only if you switch to port 465
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_APP_PASSWORD,
   },
 });
+
